3f9741e884342342a480f42f043d2fe7f5127c10
tbsexton <tbsexton@asu.edu>
2017-12-14 23:16:07 -0500

    finished BO and result plots

--- hmm.py
+++ hmm.py
@@ -5,6 +5,8 @@ import pandas as pd
 from itertools import compress
 import os
 from sklearn.preprocessing import StandardScaler
+from sklearn.covariance import EllipticEnvelope
+from sklearn.metrics import fbeta_score
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from scipy import stats
@@ -52,7 +54,7 @@ class RollingHMM(object):
     def train(self, X, **kwargs):
         base_kwargs = {
             'verbose': True,
-            'n_jobs': 4,
+            # 'n_jobs': 2,
             'n_components': self.n_states
         }
         base_kwargs.update(kwargs)
@@ -79,18 +81,28 @@ class RollingHMM(object):
         return r.apply(self.model.log_probability)
 
     def unsupervised_anom(self):
-        l = self.like()
+        l = self.like().copy()
         # if np.any(l.isna()):
         #     return 'NaN'
 
         # p = l.rolling(self.window).min().drop_duplicates()
         # dp = p.diff()
-        n_anom = int(self.target.sum())
+
         # # print(dp)
         # min_idx = np.argpartition(dp, n_anom)[:n_anom]
         # return dp.iloc[min_idx]
-        min_idx = np.argpartition(l, n_anom)[:n_anom]
-        return l.iloc[min_idx]
+        outl_frac = self.target.sum()/self.target.shape[0]
+        lof = EllipticEnvelope(contamination=outl_frac).fit(l.values.reshape(-1, 1))
+        outl = lof.predict(l.values.reshape(-1, 1))
+
+        outl = pd.Series(data=outl, index=l.index)
+
+        outl = outl.map({-1: 1, 1: 0})
+
+        # l.outl = outl.outl.rolling('8h').max()
+        # min_idx = np.argpartition(l, n_anom)[:n_anom]
+        # min_idx =l.
+        return outl
 
     def rmse(self):
         anom = self.unsupervised_anom()
@@ -99,6 +111,13 @@ class RollingHMM(object):
         pred = anom.sort_index().index.values
         actual = self.target[self.target == 1].sort_index().index.values
         return np.sqrt((np.array([i.item()*1e-13 for i in (pred-actual)])**2).mean())
+
+    def fscore(self, b=2):
+        outl = self.unsupervised_anom().rolling(self.window).max()
+        true = self.target.rolling(self.window).max()
+        score=fbeta_score(true, outl, beta=b)
+        # print(f'Score: {score}')
+        return score
 
 
 class TurbineAnomalyHMM(object):
@@ -118,7 +137,7 @@ class TurbineAnomalyHMM(object):
                             pd.read_csv(os.path.join(data_dir, 'data_7664.csv'),
                                         index_col=0, parse_dates=True)])
             print('creating h5...')
-            df.to_hdf(os.path.join(data_dir, 'completeDB.h5'))
+            df.to_hdf(os.path.join(data_dir, 'completeDB.h5'), 'all')
             print('done!')
         else:
             print('reading h5...')
@@ -175,23 +194,28 @@ class TurbineAnomalyHMM(object):
             experiment[sensor].fit(verbose=False)
 
         # print('HI')
-        rmse = {n: i.rmse() for n, i in list(experiment.items())}
-        obj = stats.hmean(np.array(list(rmse.values())))
-        case_study_fname = 'o{:.1f}w{}s{}_optHMM.pkl'.format(obj, anomaly_window, n_states)
+
+        # rmse = {n: i.rmse() for n, i in list(experiment.items())}
+        fscore = {n: i.fscore() for n, i in list(experiment.items())}
+        # obj = stats.hmean(np.array(list(rmse.values())))
+        obj = 100.*np.array(list(fscore.values())).mean()
+        print(f'f = {obj:.2f}')
+        case_study_fname = 'opt{:.1f}_{}_{}_hmm.pkl'.format(obj, anomaly_window, n_states)
         # print('saving iteration HMM pkl...')
         with open(os.path.join(self.dir, 'results', case_study_fname), 'wb') as f:
             pickle.dump(experiment, f)
         # print('done!')
         return obj
 
-    def optimize(self, domain, max_iter):
+    def optimize(self, domain, max_iter, X=None, Y=None):
         import GPyOpt
         myBopt = GPyOpt.methods.BayesianOptimization(f=self.objective,  # Objective function
                                                      domain=domain,  # Box-constrains of the problem
                                                      exact_feval=False,
-                                                     # X=X, Y=Y,
+                                                     maximize=True,
+                                                     X=X, Y=Y,
                                                      initial_design_type='latin',
-                                                     # num_cores=4,
+                                                     # num_cores=2,
                                                      model_type='GP',
                                                      acquisition_type='LCB')
         myBopt.run_optimization(max_iter)
@@ -201,10 +225,17 @@ class TurbineAnomalyHMM(object):
 if __name__ == "__main__":
     data_dir = os.path.join('.', 'data')
     study = TurbineAnomalyHMM(data_dir, compress_window='4h')
-    domain = [{'name': 'anomaly_window', 'type': 'discrete', 'domain': range(8, 49)},
-              {'name': 'n_states', 'type': 'discrete', 'domain': range(3, 11)}]
+    domain = [{'name': 'anomaly_window', 'type': 'discrete', 'domain': range(4, 49)},
+              {'name': 'n_states', 'type': 'discrete', 'domain': range(2, 5)}]
+    # X = np.array([[28,3],[34,3],[34,4], [26,6], [16,4], [30,3]])
+    # Y = np.array([3.4, 5.4, 5.5, 7.9, 8.3, 4.4]).reshape(-1, 1)
     myBopt = study.optimize(domain, 20)
     myBopt.plot_acquisition(filename='acq.png')
     myBopt.plot_convergence(filename='conv.png')
+
+    opt_fname = 'myBot_4h3s.pkl'
+    print('saving BO pkl...')
+    with open(os.path.join(data_dir, 'results', opt_fname), 'wb') as f:
+        pickle.dump(myBopt, f)
 
     print('done!')
